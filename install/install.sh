a7bf4e46346dea22f78763326625d036e47b29d5
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-06-22 16:40:37 +0200

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,1 +1,27 @@
+cd /opt
 
+sudo apt-get update
+
+# terraform
+
+sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp-archive-keyring.gpg
+sudo mv hashicorp-archive-keyring.gpg /usr/share/keyrings/
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update
+sudo apt-get install terraform
+
+# aws cli
+
+sudo apt-get install -y curl unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip
+rm -rf aws
+
+
+
+# poetry
+
+ apt install python3-poetry -y
