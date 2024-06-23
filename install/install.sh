36d48dfb26fcc978ce0f28c325c93ad8606f2d20
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-06-23 17:24:42 +0200

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -8,8 +8,8 @@ sudo apt-get install -y gnupg software-properties-common
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp-archive-keyring.gpg
 sudo mv hashicorp-archive-keyring.gpg /usr/share/keyrings/
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt-get update
-sudo apt-get install terraform
+sudo apt-get update -y
+sudo apt-get install terraform -y
 
 # aws cli
 
