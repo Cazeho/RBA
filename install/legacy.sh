59c0cce42509433cc2589410978e0ada1b724290
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-06-24 17:32:53 +0200

    Update legacy.sh

--- install/legacy.sh
+++ install/legacy.sh
@@ -1,29 +1,61 @@
-cd /opt
-
-sudo apt-get update -y
-
-# terraform
-
-sudo apt-get install -y gnupg software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor > hashicorp-archive-keyring.gpg
-sudo mv hashicorp-archive-keyring.gpg /usr/share/keyrings/
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt-get update -y
-sudo apt-get install terraform -y
-
-# aws cli
-
-sudo apt-get install -y curl unzip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-rm awscliv2.zip
-rm -rf aws
-
-apt update -y
-
-# attack range
-
-cd /opt
-git clone https://github.com/splunk/attack_range.git
-cd attack_range
+  cd /opt
+    2  apt-get update
+    3  apt-get install -y python3.8 git unzip python3-pip curl
+    4  add-apt-repository ppa:deadsnakes/ppa
+    5  apt-get install -y python3.8 git unzip python3-pip curl
+    6  git clone https://github.com/splunk/attack_range.git
+    7  cd attack_range
+    8  curl -s https://releases.hashicorp.com/terraform/1.1.8/terraform_1.1.8_linux_amd64.zip -o terraform.zip && unzip terraform.zip && mv terraform /usr/local/bin/
+    9  ls
+   10  cd terraform
+   11  ls
+   12  cd
+   13  cd /opt/
+   14  rm -rf attack_range/
+   15  curl -s https://releases.hashicorp.com/terraform/1.1.8/terraform_1.1.8_linux_amd64.zip -o terraform.zip && unzip terraform.zip && mv terraform /usr/local/bin/
+   16  git clone https://github.com/splunk/attack_range.git
+   17  cd attack_range/
+   18  ls
+   19  cd ..
+   20  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+   21  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+   22  apt-get update && sudo apt-get install packer
+   23  terraform
+   24  sudo apt-get install -y curl unzip
+   25  aws
+   26  apt-get install -y curl unzip
+   27  ls
+   28  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+   29  unzip awscliv2.zip
+   30  sudo ./aws/install
+   31  rm -rf aws
+   32  aws
+   33  aws configure
+   34  curl -sSL https://install.python-poetry.org/ | python -
+   35  curl -sSL https://install.python-poetry.org/ | python3.8 -
+   36  poetry
+   37  python3.8
+   38  python --version
+   39  ls
+   40  cat poetry-installer-error-xb91so7k.log
+   41  pip install
+   42*
+   43  curl -sSL https://install.python-poetry.org/ | python3.8 -
+   44  poetry
+   45  apt install python
+   46  apt install python3
+   47  curl -sSL https://install.python-poetry.org/ | python3 -
+   48  cd attack_range/
+   49  poetry shell
+   50  cd ..
+   51  curl -sSL https://install.python-poetry.org/ | python3 -
+   52  poetry
+   53  ls
+   54  which poetry
+   55  curl -sSL https://install.python-poetry.org/ | python3 -
+   56  poetry
+   57  apt install python3-poetry
+   58  cd attack_range/
+   59  ls
+   60  poetry shell
+   61  history
