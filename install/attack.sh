6cd149b26e567f9771e375195d492f0ad0144a4a
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-06-24 18:05:58 +0200

    Update attack.sh

--- install/attack.sh
+++ install/attack.sh
@@ -1,8 +1,11 @@
 apt update -y
 apt install -y python3 python3-pip
 apt install -y python3-impacket
-apt install -y python3.11-venv
-pip install kerbrute
+apt install -y python3-poetry
+
+#apt install -y python3.11-venv
+
+
 
 cd /opt
 mkdir attack
@@ -16,9 +19,13 @@ cd /opt/attack
 
 git clone https://github.com/Pennyw0rth/NetExec
 cd NetExec
-python3 -m venv .
-source bin/activate
-pip install .
+poetry shell
+poetry install
+#python3 -m venv .
+#source bin/activate
+#pip install .
+
+pip install kerbrute
 
 # install Villain C2
 
@@ -32,5 +39,6 @@ pip3 install -r requirements.txt
 # install Binary (Mimikatz...etc)
 
 cd /opt/attack
-
+mkdir bin
+cd bin
 wget -O mimikatz.exe "https://gitlab.com/kalilinux/packages/mimikatz/-/raw/kali/master/x64/mimikatz.exe?ref_type=heads"
