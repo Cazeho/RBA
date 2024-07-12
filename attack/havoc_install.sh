0e0d175eeaf4ca8ec2d46b29c40dddddbe7bbe76
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-07-12 18:00:58 +0200

    Create havoc_install.sh

---
+++ attack/havoc_install.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/HavocFramework/Havoc.git
+cd Havoc
+sudo apt install -y git build-essential apt-utils cmake libfontconfig1 libglu1-mesa-dev libgtest-dev libspdlog-dev libboost-all-dev libncurses5-dev libgdbm-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev mesa-common-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5 libqt5websockets5-dev qtdeclarative5-dev golang-go qtbase5-dev libqt5websockets5-dev python3-dev libboost-all-dev mingw-w64 nasm
+
+cd teamserver
+go mod download golang.org/x/sys
+go mod download github.com/ugorji/go
+cd ..
+make ts-build
+make client-build
+./havoc server --profile ./profiles/havoc.yaotl -v --debug
