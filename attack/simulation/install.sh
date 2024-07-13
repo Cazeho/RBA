dc1d87d24e95b51d2b4214c416f3e3f4dbeaed9e
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-07-13 19:43:17 +0200

    Update install.sh

--- attack/simulation/install.sh
+++ attack/simulation/install.sh
@@ -3,6 +3,7 @@ apt install -y python3-poetry
 apt install -y python3-pip
 apt install -y evil-winrm
 apt install -y villain
+apt install -y python3-impacket
 apt install -y jq
 
 cd /opt/RBA/attack/simulation
