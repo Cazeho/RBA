7f70ede37ad302603a6e1cc3deb65903ab14a4dd
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-07-31 17:50:15 +0200

    Update install.sh

--- automatisation/install.sh
+++ automatisation/install.sh
@@ -5,5 +5,7 @@ cd /opt
 mkdir gitlab
 cd gitlab
 
+curl -s https://raw.githubusercontent.com/Cazeho/download_project/main/docker_ubuntu.sh | bash
+
 docker compose up -d
 docker exec -it gitlab gitlab-rails runner "user = User.find_by(username: 'root'); user.password = 'Password123!'; user.password_confirmation = 'Password123!'; user.save!"
