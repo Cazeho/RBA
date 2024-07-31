5f26d9c1c63afc45a9727e7e61e19a791576f9fa
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-07-31 17:49:10 +0200

    Create install.sh

---
+++ automatisation/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /opt
+
+mkdir gitlab
+cd gitlab
+
+docker compose up -d
+docker exec -it gitlab_web_1 gitlab-rails runner "user = User.find_by(username: 'root'); user.password = 'Password123!'; user.password_confirmation = 'Password123!'; user.save!"
