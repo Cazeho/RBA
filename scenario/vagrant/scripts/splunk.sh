bda6e0e62dd11af834ee8ed52b146999af3531d6
Cazeho <58745332+Cazeho@users.noreply.github.com>
2025-01-12 02:36:14 +0100

    Update splunk.sh

--- scenario/vagrant/scripts/splunk.sh
+++ scenario/vagrant/scripts/splunk.sh
@@ -7,5 +7,5 @@ tar -xzf splunk-9.0.4.1-419ad9369127-Linux-x86_64.tgz -C /opt
 echo 'export PATH=$PATH:/opt/splunk/bin' >> ~/.bashrc
 source ~/.bashrc
 
-splunk start --accept-license --answer-yes --no-prompt --seed-passwd '2833632791'
+splunk start --accept-license --answer-yes --no-prompt --seed-passwd 'admin123!'
 splunk enable boot-start
